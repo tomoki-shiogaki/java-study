de9dd3db4d7cc1730f9edd547a24f75f6fbf292c
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-22 16:13:11 +0900

    add keisan.java

---
+++ koreeda/00/keisan.java
@@ -0,0 +1,25 @@
+package com.petitmonte;
+
+public class keisan {
+
+	public static void main(String[] args) {
+		//No.1 12345+23456
+		System.out.println(12345+23456);
+		//No.2 12345‚ð7‚ÅŠ„‚Á‚½‚ ‚Ü‚è
+		System.out.println(12345%7);
+		//No.3
+		int a = 123;
+		System.out.println("Your number is " +a);
+		//No.4
+		int b = 123;
+		System.out.println(b*3);
+		//No.5
+		int c = 7;
+		System.out.println(b+c);
+		System.out.println(b-c);
+		System.out.println(b*c);
+		System.out.println(b/c);
+		System.out.println(b%c);
+	}
+
+}
