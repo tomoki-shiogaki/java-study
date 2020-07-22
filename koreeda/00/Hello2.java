8bde9276dfd01fef2b67410ccce0b77507c502bf
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-22 16:23:46 +0900

    add Hello2.java

--- koreeda/00/Hello2.java
+++ koreeda/00/Hello2.java
@@ -5,7 +5,6 @@ import javax.swing.JFrame;
 public class Hello2 {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
 		 JFrame frame = new JFrame("Hello World!");
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.setSize(300, 300);
