b57d220db2a11022a37993446262e8f01c1166ab
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-22 16:36:13 +0900

    add absolute.java

---
+++ koreeda/00/absolute.java
@@ -0,0 +1,18 @@
+package com.petitmonte;
+
+public class absolute {
+
+	public static void main(String[] args) {
+		//No.10
+		int num1 = 100;
+		int num2 = -100;
+		
+		num1 = Math.abs(num1);
+		num2 = Math.abs(num2);
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+	}
+
+}
