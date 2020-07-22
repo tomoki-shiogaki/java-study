e549b799888e5258db7613de69c880ee5ca22ac0
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-22 16:28:45 +0900

    add prctice1.java

---
+++ koreeda/00/practice1.java
@@ -0,0 +1,21 @@
+package com.petitmonte;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		
+		//No.6
+		int a = 0;
+		if (a == 0) {
+			System.out.println("zero");
+		}
+		
+		//No.7
+		int b = 8;
+		if(b ==0) {
+			System.out.println("zero");
+		} else {
+			System.out.println("not zero");
+		}
+	}
+}
