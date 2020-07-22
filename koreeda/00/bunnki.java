56a3096774abf43f9cfcd93e38d0cf3a591278be
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-22 16:33:10 +0900

    add bunnki.java

---
+++ koreeda/00/bunnki.java
@@ -0,0 +1,18 @@
+package com.petitmonte;
+
+public class bunnki {
+
+	public static void main(String[] args) {
+		//No.8,9
+		int a = 7;
+		if(a == 0 ) {
+			System.out.println("zero");
+		}else if (a > 0) {
+			System.out.println("positive");
+		}else {
+			System.out.println("negative");
+		}
+		
+	}
+
+}
