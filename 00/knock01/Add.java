cd95e480737cbd101c6c1c52f68f67155c08ab23
Miyu Akiyama <miyu-akiyama@novasytem.onmicrosoft.com>
2020-07-28 11:03:22 +0900

    remove knock00/HelloWorld.java

--- 00/knock01/Add.java
+++
@@ -1,8 +0,0 @@
-package knock01;
-
-public class Add {
-	public static void main(String[] args) {
-		int a = 12345 + 23456;
-		System.out.println(a);
-	}
-}
--- 00/knock04/Mul.java
+++
@@ -1,8 +0,0 @@
-package knock04;
-
-public class Mul {
-	public static void main(String[] args) {
-		int a = 123;
-		System.out.println(a * 3);
-	}
-}
