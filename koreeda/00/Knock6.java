e547714b5df8c68533fc833379dbc990f32ab684
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-29 17:14:09 +0900

    rewrite Knock06

---
+++ koreeda/00/Knock6.java
@@ -0,0 +1,19 @@
+package com.petitmonte;
+
+import java.util.Scanner;
+
+public class Knock6 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("input number: ");
+		int num = scanner.nextInt();
+		
+		
+		System.out.println("zero");
+		
+		scanner.close();
+
+	}
+
+}
