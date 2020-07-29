a8ceae8e9b52b52183268cb208defc2f249dbca3
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-29 16:11:03 +0900

    rewrite Knock01

--- koreeda/00/keisan.java
+++ koreeda/00/keisan.java
@@ -4,22 +4,10 @@ public class keisan {
 
 	public static void main(String[] args) {
 		//No.1 12345+23456
-		System.out.println(12345+23456);
-		//No.2 12345‚ð7‚ÅŠ„‚Á‚½‚ ‚Ü‚è
-		System.out.println(12345%7);
-		//No.3
-		int a = 123;
-		System.out.println("Your number is " +a);
-		//No.4
-		int b = 123;
-		System.out.println(b*3);
-		//No.5
-		int c = 7;
-		System.out.println(b+c);
-		System.out.println(b-c);
-		System.out.println(b*c);
-		System.out.println(b/c);
-		System.out.println(b%c);
+		int a = 12345;
+		int b = 23456;
+		int c = a +b;
+		System.out.println("12345 + 23456 = " + c);
 	}
 
 }
