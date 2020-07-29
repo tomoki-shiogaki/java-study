f10e60b5467ca8274f8a7be31af2a47e4d252f6c
Naoki Koreeda <naoki-koreeda@novasytem.onmicrosoft.com>
2020-07-29 16:24:32 +0900

    rewrite Knock02

---
+++ koreeda/00/knock2.java
@@ -0,0 +1,13 @@
+package com.petitmonte;
+
+public class knock2 {
+
+	public static void main(String[] args) {
+		int a = 12345;
+		int b = 7;
+		int c = a % b;
+				
+		System.out.println("12345 €@7‚Ì—]‚è‚Í@" + c);
+	}
+
+}
